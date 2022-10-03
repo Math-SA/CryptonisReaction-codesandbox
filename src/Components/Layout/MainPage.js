05b0582a47d50a55ecc4906268b088dd6d46b5d0
Matheus Assis <matheus.s.assis@gmail.com>
2022-10-03 17:46:35 +0000

    Add Coin Search form

--- src/Components/Layout/MainPage.js
+++ src/Components/Layout/MainPage.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import Header from "../Header";
 import CoinSearch from "../CoinSearch";
+import Footer from "../Footer";
 
 function MainPage() {
   return (
     <div>
       <Header></Header>
       <CoinSearch></CoinSearch>
+      <Footer></Footer>
     </div>
   );
 }
